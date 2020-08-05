594adc252d118f8b37de14bb60a98cbaf2d9bce0
Julio Andrade <JoulesCESAR@users.noreply.github.com>
2020-08-05 16:14:09 -0500

    add

---
+++ derivate_1.py
@@ -0,0 +1,18 @@
+# Script for evaluation of first derivate at point x
+import numpy as np 
+import sys 
+
+def diff(f, x, h):
+    return (f(x + h) - f(x)) / float(h)
+
+x = eval(sys.argv[1])
+h = eval(sys.argv[2])
+
+approx_deriv = diff(np.sin, x, h)
+exact_deriv = np.cos(x)
+print(f"The approximated value is: {approx_deriv}")
+print(f"The exact value is: {exact_deriv}")
+print(f"The error is: {exact_deriv - approx_deriv}")
+
+
+# For runnig...evaluate on terminal: python derivate1.py x h

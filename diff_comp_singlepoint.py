8fd5e27f6c0718beddced6aee41367bbfd53908c
Julio Andrade <JoulesCESAR@users.noreply.github.com>
2020-09-21 21:20:05 -0500

    add
    add

---
+++ diff_comp_singlepoint.py
@@ -0,0 +1,32 @@
+# Script of derivate computation several h's
+# single point method
+import numpy as np
+import matplotlib.pyplot as plt
+
+def dfdx(f,x,h):
+    return (f(x + h) - f(x - h))/(2*h)
+
+n = 200
+x = np.linspace(0, np.pi /50, n + 1)
+h1 = (np.pi /50 )/ n
+epsilon = 5e-324
+ho = np.sqrt( (4*epsilon*100)/(10000))
+
+
+def sin100x(x):
+    return np.sin(100*x)
+
+dydx_1 = dfdx(sin100x,x,h1)
+dydx_2 = dfdx(sin100x,x,ho)
+
+dYdx = 100*np.cos(100*x)
+
+#plt.figure(figsize=(12,5))
+plt.plot(x,dydx_1,'.',label='Approx with adjusting h')
+plt.plot(x,dydx_2,'.',label='Approx with optimal h')
+
+plt.plot(x,dYdx,'b',label='Exact Value')
+
+plt.title('Derivative of y = cos(100x)')
+plt.legend(loc='best')
+plt.show()
